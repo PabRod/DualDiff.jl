c86946ca1bc4b05959d87dd210875c1ba10930c5
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2023-08-09 16:27:09 +0200

    Specify output types

--- src/Dual.jl
+++ src/Dual.jl
@@ -6,14 +6,14 @@ struct Dual
     dx::Real
 
     """ Default constructor """
-    function Dual(x::Real, dx::Real=0)
+    function Dual(x::Real, dx::Real=0)::Dual
         new(x, dx)
     end
 
     """ If passed a Dual, just return it
     
     This will be handy later """
-    function Dual(x::Dual)
+    function Dual(x::Dual)::Dual
         return x
     end
 
@@ -27,35 +27,35 @@ import Base: +, -, *, /, \
 +(z::Dual) = return z
 -(z::Dual) = return Dual(-z.x, -z.dx)
 
-function +(self::DualNumber, other::DualNumber)
+function +(self::DualNumber, other::DualNumber)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     return Dual(self.x + other.x, self.dx + other.dx)
 end
 
-function -(self::DualNumber, other::DualNumber)
+function -(self::DualNumber, other::DualNumber)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     return self + (-other)
 end
 
-function *(self::DualNumber, other::DualNumber)
+function *(self::DualNumber, other::DualNumber)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x * other.x
     dy = self.dx * other.x + self.x * other.dx
     return Dual(y, dy)
 end
 
-function /(self::DualNumber, other::DualNumber)
+function /(self::DualNumber, other::DualNumber)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x / other.x
     dy = (self.dx * other.x - self.x * other.dx) / (other.x)^2
     return Dual(y, dy)
 end
 
-function \(self::DualNumber, other::DualNumber)
+function \(self::DualNumber, other::DualNumber)::Dual
     return other / self
 end
 
-function Base.:^(self::Dual, other::Real)
+function Base.:^(self::Dual, other::Real)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x^other.x
     dy = other.x * self.x^(other.x - 1) * self.dx
