9ec7b8576e31d60fec4a22b5f5c8af2c4c369b62
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2023-08-09 16:07:27 +0200

    Define Number type

--- src/Dual.jl
+++ src/Dual.jl
@@ -1,6 +1,6 @@
 export Dual
 
-""" Structure representing a Dual number """
+""" Structure representing a Dual DualNumber """
 struct Dual
     x::Real
     dx::Real
@@ -20,36 +20,38 @@ struct Dual
 end
 
 ## Redefine base operators
+const DualNumber = Union{Dual, Number}
+
 import Base: +, -, *, /, \
 
 +(z::Dual) = return z
 -(z::Dual) = return Dual(-z.x, -z.dx)
 
-function +(self::Union{Real,Dual}, other::Union{Real,Dual})
+function +(self::DualNumber, other::DualNumber)
     self, other = Dual(self), Dual(other) # Coerce into Dual
     return Dual(self.x + other.x, self.dx + other.dx)
 end
 
-function -(self::Union{Real,Dual}, other::Union{Real,Dual})
+function -(self::DualNumber, other::DualNumber)
     self, other = Dual(self), Dual(other) # Coerce into Dual
     return self + (-other)
 end
 
-function *(self::Union{Real,Dual}, other::Union{Real,Dual})
+function *(self::DualNumber, other::DualNumber)
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x * other.x
     dy = self.dx * other.x + self.x * other.dx
     return Dual(y, dy)
 end
 
-function /(self::Union{Real,Dual}, other::Union{Real,Dual})
+function /(self::DualNumber, other::DualNumber)
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x / other.x
     dy = (self.dx * other.x - self.x * other.dx) / (other.x)^2
     return Dual(y, dy)
 end
 
-function \(self::Union{Real,Dual}, other::Union{Real,Dual})
+function \(self::DualNumber, other::DualNumber)
     return other / self
 end
 
