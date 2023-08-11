3ce68633ec92d4f8e9dc009132a5cb3d65ff739f
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2023-08-11 15:23:37 +0200

    Generalized decorator

--- src/decorators.jl
+++ src/decorators.jl
@@ -3,8 +3,23 @@ export autodifferentiable
 """
     autodifferentiable(f)
 
-Just some syntactic sugar. Allows using f(x) instead of f(Dual(x, 1))
+  Seamlessly turns a given function 
+    f = x -> expr(x) 
+    into
+    f = x -> expr(Dual(x, 1))
+
+    How to use it:
+    f = autodifferentiable(f)
 """
 function autodifferentiable(f)
-    return x -> f(Dual(x, 1))
+
+    function decorated(x::Number)
+        return f(Dual(x, 1))
+    end
+
+    function decorated(x::Dual)
+        return f(x)
+    end
+
+    return decorated
 end
