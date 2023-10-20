d0eb9b48402cf1f9073cbf7dc8f2a3d8ee991e96
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2023-10-20 16:17:54 +0200

    Partial implementation of Dual as inherited from Number

--- src/Dual.jl
+++ src/Dual.jl
@@ -1,20 +1,13 @@
 export Dual
 
 """ Structure representing a Dual DualNumber """
-struct Dual
+struct Dual <: Number
     x::Real
     dx::Real
 
     """ Default constructor """
     function Dual(x::Real, dx::Real=0)::Dual
         new(x, dx)
-    end
-
-    """ If passed a Dual, just return it
-    
-    This will be handy later """
-    function Dual(x::Dual)::Dual
-        return x
     end
 
 end
@@ -36,38 +29,68 @@ import Base: ==, !=, >, >=, <, <=
 ## Algebraic operators
 import Base: +, -, *, /, \, ^
 
-+(z::Dual) = z
 -(z::Dual) = Dual(-z.x, -z.dx)
 
-function +(self::DualNumber, other::DualNumber)::Dual
+function +(self::Dual, other::Dual)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     return Dual(self.x + other.x, self.dx + other.dx)
 end
 
-function -(self::DualNumber, other::DualNumber)::Dual
+function +(self::Dual, other::Integer)::Dual
+    self, other = Dual(self), Dual(other) # Coerce into Dual
+    return Dual(self.x + other.x, self.dx + other.dx)
+end
+
+function -(self::Dual, other::Dual)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     return self + (-other)
 end
 
-function *(self::DualNumber, other::DualNumber)::Dual
+function -(self::Dual, other::Integer)::Dual
+    self, other = Dual(self), Dual(other) # Coerce into Dual
+    return self + (-other)
+end
+
+function *(self::Dual, other::Dual)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x * other.x
     dy = self.dx * other.x + self.x * other.dx
     return Dual(y, dy)
 end
 
-function /(self::DualNumber, other::DualNumber)::Dual
+function *(self::Dual, other::Integer)::Dual
+    self, other = Dual(self), Dual(other) # Coerce into Dual
+    y = self.x * other.x
+    dy = self.dx * other.x + self.x * other.dx
+    return Dual(y, dy)
+end
+
+function *(self::Integer, other::Dual)::Dual
+    self, other = Dual(self), Dual(other) # Coerce into Dual
+    y = self.x * other.x
+    dy = self.dx * other.x + self.x * other.dx
+    return Dual(y, dy)
+end
+
+function /(self::Dual, other::Dual)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x / other.x
     dy = (self.dx * other.x - self.x * other.dx) / (other.x)^2
     return Dual(y, dy)
 end
 
-function \(self::DualNumber, other::DualNumber)::Dual
+function \(self::Dual, other::Dual)::Dual
     return other / self
 end
 
 function ^(self::Dual, other::Real)::Dual
+    self, other = Dual(self), Dual(other) # Coerce into Dual
+    y = self.x^other.x
+    dy = other.x * self.x^(other.x - 1) * self.dx
+    return Dual(y, dy)
+end
+
+function ^(self::Dual, other::Integer)::Dual
     self, other = Dual(self), Dual(other) # Coerce into Dual
     y = self.x^other.x
     dy = other.x * self.x^(other.x - 1) * self.dx
